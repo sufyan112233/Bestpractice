cd40c9a8bc0610641447aa874ec1ebf60b394b01
sufyan <sufyanpakhtoon>
2025-11-06 15:38:29 +0500

    second commmit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   return (
     <div>
       <h1>counter: {count}</h1>
+      <h2>did a github commit here</h2>
       <button onClick={() => setcount(count + 1)} className='border p-3 ml-3 cursor-pointer'>Inc</button>
       <button onClick={() => count > 0 && setcount(count - 1)} className='border p-3 ml-3 cursor-pointer'>Dec</button>
       <button onClick={() => setcount(0)} className='border p-3 ml-3 cursor-pointer'>reset</button>
