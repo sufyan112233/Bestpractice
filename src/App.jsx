7318b71ba35670d9082c7f8e16c82fc60945ebe8
sufyan112233 <sufyanpakhtoon38@gmail.com>
2025-11-06 15:24:56 +0500

    first commmit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [count, setcount] = useState(0)
+  return (
+    <div>
+      <h1>counter: {count}</h1>
+      <button onClick={() => setcount(count + 1)} className='border p-3 ml-3 cursor-pointer'>Inc</button>
+      <button onClick={() => count > 0 && setcount(count - 1)} className='border p-3 ml-3 cursor-pointer'>Dec</button>
+      <button onClick={() => setcount(0)} className='border p-3 ml-3 cursor-pointer'>reset</button>
+      <button onClick={() => setcount(count * 2)} className='border p-3 ml-3 cursor-pointer'>double</button>
+    </div>
+  );
+}
+
+export default App;
